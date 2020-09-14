0521eb5b733a6cb7ba8ee2acf485a1bd1e218e48
morphIsmail <ismail_2016@mail.ru>
2020-09-14 13:07:25 +0300

    Menu on javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    /* Плавная прокрутка меню */
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+    /* Мобильное меню */
+    $('.menu__burger').click(function() {
+        $('.menu').toggle(500);
+        $(this).toggleClass('close');
+    });
+});
