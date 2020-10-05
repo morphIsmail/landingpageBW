0bbc9eeb91f07bcd770b1e642cd045b567e5541f
morphIsmail <ismail_2016@mail.ru>
2020-10-05 19:18:32 +0300

    fix bug with menu

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
         },500);
         $('nav a[href^="#"]').parent().removeClass('active');
         $(this).parent().addClass('active');
-        $('.menu').toggle(500);
+        $('.menu__mobile .menu').toggle(500);
         $('.menu__burger').toggleClass('close');
         return false;
     });
     /* Мобильное меню */
     $('.menu__burger').click(function() {
-        $('.menu').toggle(500);
+        $('.menu__mobile .menu').toggle(500);
         $(this).toggleClass('close');
     });
 });
